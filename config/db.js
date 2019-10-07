c5dd393d470d4889f5a9028919e72a9f4d672f6b
Steven <anguelr12@hotmail.com>
2019-10-07 14:50:19 -0500

    Configuracion de la Base de Datos(Mongo)

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+//Conexion a la Base de Datos (DATABASE->variables.env)
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+
+mongoose.connection.on('error', (error) => {
+    console.log(error);
+})
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose');
+require('./config/db');
+
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const router = require('./routes');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session); //pasa la session al paquete de connect-mongo
+
+//Puerto local
+require('dotenv').config({ path: 'variables.env' });
 
 //App con express
 const app = express();
@@ -18,9 +27,20 @@ app.set('view engine', 'handlebars');
 //Cargar archivos estaticos del proyecto
 app.use(express.static(path.join(__dirname, 'public'))); //leer archivos de la carpeta public
 
+app.use(cookieParser());
+
+//Habilitar bodyParser para leer datos del formulario
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+
 //Rutas del aplicativo
 app.use('/', router());
 
 
 //Puerto a escuchar
-app.listen(5000);
+app.listen(process.env.PUERTO);
